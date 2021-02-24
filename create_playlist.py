972d5ce39b94379784c0ba9c5ee7b9cf7c353403
Signory Somsavath <ssomsavath@gmail.com>
2021-02-23 23:12:43 -0600

    More Secrets

--- create_playlist.py
+++ create_playlist.py
@@ -4,10 +4,17 @@
 # Step 4: Create Search for the Song 
 # Step 5: Add the Song to the spotify playlist
 
+import json
+import requests
+from secrets import spotify_user_id
+from secrets import spotify_token
+
+
 class CreatePlaylist:
 
     def __init__(self):
-        pass
+        self.user_id = spotify_user_id
+        self.spotify_tokens = spotify_token
 
     def get_youtube_client(self):
         pass
@@ -16,7 +23,23 @@ class CreatePlaylist:
         pass
     
     def create_playlist(self):
-        pass
+
+        request_body = json.dumps({
+            "name": "Liked YouTube",
+            "description": "Liked Videos on YouTube",
+            "public": True
+            })
+        
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(self.user_id)
+        response = requests.post(
+            query,
+            data= request_body,
+            headers= {
+                "Content-Type: application/json",
+                "Authorization: Bearer {}".format(spotify_token)
+            }
+        )
+
 
     def get_spotify_uri(self):
         pass
